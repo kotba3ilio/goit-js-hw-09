115977cca276a841c4bfdab0f78ba5d5dd3b5aac
Dmytro Melnychenko <31704436+kotba3ilio@users.noreply.github.com>
2024-12-28 19:30:57 +0200

    homework-9

---
+++ src/js/2-form.js
@@ -0,0 +1,30 @@
+const localStorageKey = 'feedback-form-data';
+const formData = {
+  email: '',
+  message: ''
+}
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+ document.querySelector('.feedback-form').addEventListener('load', () => {
+  const formDataTmp = JSON.parse(localStorage.getItem(localStorageKey));
+  if(formDataTmp){
+    formData.email = formDataTmp.email;
+    formData.message = formDataTmp.message;
+  }
+
+  for(const key in formData) {
+    feedbackForm.elements[key] = formData[key];
+  }
+ });
+
+ feedbackForm.addEventListener('change', (event) => {
+  const { target: element } = event;
+  formData[element.name] = element.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+ });
+
+ feedbackForm.addEventListener('submit', () => {
+  localStorage.removeItem(localStorageKey);
+  feedbackForm.reset();
+ });
